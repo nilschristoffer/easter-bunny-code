691e85aed8ae14931fbaa9b18e5eb978568644ca
nilschristoffer <nilschristofferlindahl@gmail.com>
2021-03-31 21:01:46 +0200

    Adding solution for task day 3

---
+++ EasterCodeBunny.Tests/Task3.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace EasterCodeBunny.Tests
+{
+    public class Task3
+    {
+        [Fact]
+        public void Should_Result_In_3_Permutations_If_One_Number_With_Three_Letters()
+        {
+            // Act
+            var actual = EasterCodeBunny.Task3.T9.Permutations("2").Count;
+
+            // Assert
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void Should_Result_In_12_Permutations_If_Two_Numbers_With_Three_And_Four_Letters()
+        {
+            // Act
+            var actual = EasterCodeBunny.Task3.T9.Permutations("27").Count;
+
+            // Assert
+            Assert.Equal(12, actual);
+        }
+
+        [Fact]
+        public void Should_Result_In_27_Permutations_If_Three_Numbers_With_Three_Letters_Each()
+        {
+            // Act
+            var actual = EasterCodeBunny.Task3.T9.Permutations("234").Count;
+
+            // Assert
+            Assert.Equal(27, actual);
+        }
+    }
+}
---
+++ EasterCodeBunny/Task3/T9.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterCodeBunny.Task3
+{
+    public static class T9
+    {
+        public static IList<string> Permutations(string numbers)
+        {
+            var permutations = new List<string>() { "" };
+            foreach(char n in numbers)
+            {
+                var newPermutations = new List<string>();
+                try
+                {             
+                    foreach(var letter in translations[n])
+                    {
+                        foreach(var permutation in permutations)
+                        {
+                            newPermutations.Add(permutation + letter);
+                        }
+                    }
+                    permutations = newPermutations;
+                }
+                catch
+                {
+                    throw new Exception("Invalid sequence");
+                }
+            }
+            return permutations;
+        }
+
+        private static readonly Dictionary<char, string> translations = new Dictionary<char, string>()
+        {
+            { '2', "abc" }, 
+            { '3', "def" }, 
+            { '4', "ghi" }, 
+            { '5', "jkl" }, 
+            { '6', "mno" }, 
+            { '7', "pqrs" }, 
+            { '8', "tuv" }, 
+            { '9', "wxyz" } 
+        };
+    }
+}
